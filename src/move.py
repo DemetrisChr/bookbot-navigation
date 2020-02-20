d8317800b8e61ecb3071d8cd53a3f77c23adef5a
Christopher <cj1mckay@gmail.com>
2020-02-20 18:06:08 +0000

    Script works

--- src/move.py
+++ src/move.py
@@ -2,30 +2,52 @@
 import roslib
 import rospy
 from geometry_msgs.msg import *
+from std_msgs.msg import Header
 
 def gen_pose():
-    pub = rospy.Publisher('initialpose', PoseWithCovarianceStamped)
-    rospy.init_node('initial_pose'); #, log_level=roslib.msg.Log.INFO)
-    rospy.loginfo("Setting Pose")
+    # Latched to ensure message is sent when publisher and subscriber are ready
+    pub = rospy.Publisher('initialpose', PoseWithCovarianceStamped, latch=True)
+   # rospy.init_node('initial_pose'); #, log_level=roslib.msg.Log.INFO)
+   # rospy.loginfo("Setting Pose")
 
-    p   = PoseWithCovarianceStamped();
-    msg = PoseWithCovariance();
-    msg.pose = Pose(Point(-0.767, -0.953, 0.000), Quaternion(0.000, 0.000, -0.0149, 0.9999));
-    msg.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942];
-    p.pose = msg;
-    pub.publish(p);
+    p   = PoseWithCovarianceStamped()
+    h = Header()
+    h.frame_id = 'map'
+    h.stamp = rospy.Time.now()
+
+
+    p.header = h
+
+    msg = PoseWithCovariance()
+    msg.pose = Pose(Point(0.805000007153, 1.19999969006, 0), Quaternion(0, 0, 0, 1))
+    msg.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]
+    p.pose = msg
+
+    rospy.Rate(1).sleep()
+    pub.publish(p)
 
 def publish_nav_goal():
     #Currently publishes a set position as the goal
-    publisher = rospy.Publisher('move_base_simple/goal', PoseStamped)
+    publisher = rospy.Publisher('move_base_simple/goal', PoseStamped, latch=True)
     p = PoseStamped()
-    p.pose = Pose(Point(-0.767, -0.953, 0.000), Quaternion(0.000, 0.000, -0.0149, 0.9999))
+    h = Header()
+    h.frame_id = 'map'
+    h.stamp = rospy.Time.now()
+
+
+    p.header = h
+    p.pose = Pose(Point(-0.247143253684, -1.25660061836, 0.000), Quaternion(0.000, 0.000, -0.0149, 0.9999))
+    
+    rospy.Rate(1).sleep()
     publisher.publish(p)
 
 if __name__ == '__main__':
     try:
+        rospy.init_node('initial_pose'); #, log_level=roslib.msg.Log.INFO)
+        rospy.loginfo("Setting Pose")
+
         gen_pose()
-        publish_nav_goal()
+        #publish_nav_goal()
         rospy.loginfo("done")
-    except Exception, e:
-        print "error: ", e
+    except Exception as e:
+        print("error: ", e)
