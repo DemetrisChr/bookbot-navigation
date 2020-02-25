2a0985c1fbc8270a81c48607f8285489fcc20097
Christopher <cj1mckay@gmail.com>
2020-02-25 13:55:09 +0000

    Added extra parameter

--- src/move.py
+++ src/move.py
@@ -5,17 +5,15 @@ import sys
 from geometry_msgs.msg import *
 from std_msgs.msg import Header
 
-def gen_pose(x,y,q):
+def gen_pose(x, y, q):
     # Latched to ensure message is sent when publisher and subscriber are ready
     pub = rospy.Publisher('initialpose', PoseWithCovarianceStamped, latch=True)
-   # rospy.init_node('initial_pose'); #, log_level=roslib.msg.Log.INFO)
-   # rospy.loginfo("Setting Pose")
+    rospy.loginfo("Setting Pose")
 
-    p   = PoseWithCovarianceStamped()
+    p = PoseWithCovarianceStamped()
     h = Header()
     h.frame_id = 'map'
     h.stamp = rospy.Time.now()
-
 
     p.header = h
 
@@ -27,9 +25,11 @@ def gen_pose(x,y,q):
     rospy.Rate(1).sleep()
     pub.publish(p)
 
-def publish_nav_goal():
-    #Currently publishes a set position as the goal
+def publish_nav_goal(x, y, q):
+    # Currently publishes a set position as the goal
     publisher = rospy.Publisher('move_base_simple/goal', PoseStamped, latch=True)
+    rospy.loginfo("Sending Navigation Goal")
+
     p = PoseStamped()
     h = Header()
     h.frame_id = 'map'
@@ -37,20 +37,26 @@ def publish_nav_goal():
 
 
     p.header = h
-    p.pose = Pose(Point(-0.247143253684, -1.25660061836, 0.000), Quaternion(0.000, 0.000, -0.0149, 0.9999))
-    
+    p.pose = Pose(Point(x, y, 0), Quaternion(0, 0, q, 1))
+
     rospy.Rate(1).sleep()
     publisher.publish(p)
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('initial_pose'); #, log_level=roslib.msg.Log.INFO)
-        rospy.loginfo("Setting Pose")
-        
-        #pass arguments:Point[0],Point[1],Quaternion[2]
+        rospy.init_node('sdp-navigator'); #, log_level=roslib.msg.Log.INFO)
+
+        # Pass arguments:Point[0],Point[1],Quaternion[2]
         myarg = rospy.myargv(argv=sys.argv)
-        gen_pose(float(myarg[1]),float(myarg[2]),float(myarg[3]))
-        #publish_nav_goal()
+        command = myarg[1]
+        x = float(myarg[2])
+        y = float(myarg[3])
+        angle = float(myarg[4])
+
+        if myarg[1] == 'pose':
+            gen_pose(x, y, angle)
+        elif myarg[1] == 'goal':
+            publish_nav_goal(x, y, angle)
         rospy.loginfo("done")
     except Exception as e:
         print("error: ", e)
