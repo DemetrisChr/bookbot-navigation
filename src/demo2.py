4aab136655e6ab3f7583f9fe9077c6b980318ffb
Christopher <cj1mckay@gmail.com>
2020-03-10 15:51:37 +0000

    Made demo 2 work - employed hack for robot to know when it reached its position

--- src/demo2.py
+++ src/demo2.py
@@ -2,31 +2,43 @@
 
 import roslib
 import rospy
+import time
 from nav_msgs.msg import Odometry
-from anctionlib_msg import GoalStatusArray
+from actionlib_msgs.msg import GoalStatusArray
 from navigation import publish_initial_pose, publish_goal
+from datetime import datetime
 
 goals = [{"x":3.21, "y":-0.38, "q": -0.71}, {"x":-0.04, "y":-0.67, "q": -0.71}, {"x":0.8, "y":1.2, "q": 0}]
 current_goal = goals.pop(0)
 
+time_since_goal_reached = time.time()
+
 
 def callback(msg):
-    global goals, current_goal
+    global goals, current_goal, time_since_goal_reached
     #print(current_goal)
 
     goalreached = False
+    #print(msg.status_list)
     if (len(msg.status_list) > 0 ):
-        goalreached = msg.status_list.text == "Goal reached." 
+        goalreached = msg.status_list[0].text == "Goal reached."
+
+        if goalreached:
+            now = time.time()
+            time_since = now - time_since_goal_reached
+            if(time_since < 8):
+                goalreached = False
+            else:
+                time_since_goal_reached = time.time()
 
     if(goalreached):
         if(len(goals) > 0):
-            print("Reached goal, waiting then moving on...")
-            rospy.Rate(2000).sleep()
+            print("Performing next move")
+            
             current_goal = goals.pop(0)
             publish_goal(**current_goal)
         else:
-            exit()
-
+            print("Goal reached but got no where to go")
 
 rospy.init_node('demo2_node')
 
