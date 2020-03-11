fd7ef14e3a0c9b98e54b45e1f8b656a631e392d6
VLemontea <siyu.liu0128@gmail.com>
2020-03-11 15:09:45 +0000

    Fixed goal reached checking

--- src/demo2.py
+++ src/demo2.py
@@ -10,33 +10,29 @@ from datetime import datetime
 
 goals = [{"x":3.21, "y":-0.38, "q": -0.71}, {"x":-0.04, "y":-0.67, "q": -0.71}, {"x":0.8, "y":1.2, "q": 0}]
 current_goal = goals.pop(0)
-
-time_since_goal_reached = time.time()
-
+index = 1
 
 def callback(msg):
-    global goals, current_goal, time_since_goal_reached
+    global goals, current_goal, index, goalstatus
     #print(current_goal)
 
     goalreached = False
     #print(msg.status_list)
     if (len(msg.status_list) > 0 ):
-        goalreached = msg.status_list[0].text == "Goal reached."
-
-        if goalreached:
-            now = time.time()
-            time_since = now - time_since_goal_reached
-            if(time_since < 8):
-                goalreached = False
-            else:
-                time_since_goal_reached = time.time()
+        msg_goal_id = msg.status_list[-1].goal_id.id.split("_")[2]
+        print(msg_goal_id)
+        if(msg.status_list[-1].status == 3 and msg_goal_id == str(index)):
+            goalreached = True
+        else:
+            goalreached = False
 
     if(goalreached):
         if(len(goals) > 0):
             print("Performing next move")
-            
+
             current_goal = goals.pop(0)
             publish_goal(**current_goal)
+            index += 1
         else:
             print("Goal reached but got no where to go")
 
