923a033512f4f87dc04c6b78767b5425a503f60a
VLemontea <siyu.liu0128@gmail.com>
2020-03-18 21:28:26 +0000

    changed Position definition

--- src/position.py
+++ src/position.py
@@ -6,10 +6,11 @@ from geometry_msgs.msg import *
 from std_msgs.msg import Header
 
 class Position():
-    def __init__(self, x, y, angle):
+    def __init__(self, x, y, z, w):
         self.x = x
         self.y = y
-        self.angle = angle
+        self.z = z
+        self.w = w
 
     def generate_stamped_message(self, covariance=False):
         h = Header()
@@ -17,12 +18,12 @@ class Position():
         h.stamp = rospy.Time.now()
 
         p = PoseStamped()
-        msg = Pose(Point(self.x, self.y, 0), Quaternion(0, 0, self.angle, 1))
+        msg = Pose(Point(self.x, self.y, 0), Quaternion(0, 0, self.z, self.w))
         
         if covariance:
             p = PoseWithCovarianceStamped()
             msg = PoseWithCovariance()
-            msg.pose = Pose(Point(self.x, self.y, 0), Quaternion(0, 0, self.angle, 1))
+            msg.pose = Pose(Point(self.x, self.y, 0), Quaternion(0, 0, self.z, self.w))
             msg.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]
 
         p.header = h
--- src/testing.py
+++ src/testing.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
-
+import roslib
+import rospy
 from position import Position
 from navigator import Navigator
 import time
 
+rospy.init_node('sdp_navigator')
+
 nav = Navigator()
 
-inital_pose = Position(0.81,1.2,1)
-goal = Position(2.21,0.79,1)
+inital_pose = Position(0.88, 1.14, 0, 1)
+goal = Position(2.9, -1.4, 0.95, 0)
 
 nav.set_initial_position(inital_pose)
+#
 time.sleep(2)
 nav.go_to(goal)
